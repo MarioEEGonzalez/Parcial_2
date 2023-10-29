9bf25fcc4f48122b516ff6ec20f7f6283ea8acfd
MarioEEGonzalez <123225215+MarioEEGonzalez@users.noreply.github.com>
2023-10-28 23:50:04 -0500

    Update main.cpp

--- ParcialI/main.cpp
+++ ParcialI/main.cpp
@@ -1,24 +1,12 @@
 #include "tablero.h"
 
-void imprimir_matriz(string** matriz, int filas, int columnas){
-    cout<<"  A B C D E F G H"<<endl;
-    for (int i = 0; i<filas; i++){
-        cout<<i<<" ";
-        for (int u = 0; u<columnas; u++ ){
-            cout<< matriz[i][u]<< " ";
-        }
-        cout<<i<<" ";
-        cout<< endl;
 
-    }
-    cout<<"  A B C D E F G H"<<endl;
-}
 
 int main()
 {
 
 
-    tablero tablero1(8,8);
-    imprimir_matriz(tablero1.matriz,8,8);
+    tablero tablero1;
+    tablero1.imprimir_matriz();
      return 0;
 }
