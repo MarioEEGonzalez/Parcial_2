43527f630497af97186922e15dbf9d694763f48f
MarioEEGonzalez <123225215+MarioEEGonzalez@users.noreply.github.com>
2023-10-28 23:50:10 -0500

    Update tablero.cpp

--- ParcialI/tablero.cpp
+++ ParcialI/tablero.cpp
@@ -1,27 +1,40 @@
 #include "tablero.h"
 
-tablero::tablero(int filas, int columnas){
+tablero::tablero(){
 
-    matriz =  new string* [filas];
+    matriz =  new char* [filas];
 
         for (int i = 0;i<filas;i++){
-            matriz[i]= new string [columnas];
+            matriz[i]= new char [columnas];
         }
 
     for (int i = 0; i<filas; i++){
         for (int u = 0; u<columnas; u++ ){
 
                 if (i==(columnas/2) && u== i || i == (columnas/2)-1 && u == i) {
-                matriz[i][u] = "*";
+                matriz[i][u] = '*';
             }
             else if (u  ==(filas/2) && i == u-1 || u  ==(filas/2)-1 && i == u+1 ){
-                matriz[i][u] = "-";
+                matriz[i][u] = '-';
             }
             else{
-                matriz[i][u]= "o";
+                matriz[i][u]= 'o';
             }
         }
     }
 }
 
+void tablero::imprimir_matriz(){
+    cout<<"  A B C D E F G H"<<endl;
+    for (int i = 0; i<filas; i++){
+        cout<<i<<" ";
+        for (int u = 0; u<columnas; u++ ){
+            cout<< matriz[i][u]<< " ";
+        }
+        cout<<i<<" ";
+        cout<< endl;
+
+    }
+    cout<<"  A B C D E F G H"<<endl;
+}
 
